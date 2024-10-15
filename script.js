7229a6425b2864d5096c0b66ae5364747aab6f54
alexc <166944852+Gulsun1@users.noreply.github.com>
2024-10-15 16:21:43 +0200

    Create initial logic for game

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
-const gridContainer = document.querySelector('#gridContainer');
+const gridContainer = document.getElementById('gridContainer');
+const content = document.querySelector('.content');
+const gridSizeSlider = document.getElementById('gridSizeSlider');
+const sliderValueDisplay = document.getElementById('sliderValue');
+
+function updateGrid() {
+    const gridSize = parseInt(gridSizeSlider.value); // Get the current value of the slider
+
+    sliderValueDisplay.textContent = gridSize;
+
+    gridContainer.innerHTML = '';
+
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+    for (let i = 0; i < (gridSize * gridSize); i++) {
+        const gridCell = document.createElement('div');
+        gridCell.classList.add('gridCell');
+        gridCell.addEventListener('mouseover', function() {
+            gridCell.style.backgroundColor = 'black';
+        });
+        gridContainer.appendChild(gridCell);
+    }
+}
+
+gridSizeSlider.addEventListener('input', updateGrid);
+
+updateGrid();
+
 
